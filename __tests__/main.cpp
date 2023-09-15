c484d7836f24918fb59ac2f6f4d0511fbd9b4dc7
reika727 <reikasuya727@gmail.com>
2023-09-15 21:21:23 +0900

    refactor __test__/main.cpp

--- __tests__/main.cpp
+++ __tests__/main.cpp
@@ -5,20 +5,15 @@
 
 int main(int argc, char *argv[])
 {
-    try {
-        if (argc != 2) {
-            throw std::runtime_error("invalid command line");
-        }
-        std::ifstream ifs(argv[1]);
-        if (!ifs) {
-            throw std::runtime_error("can't open file");
-        }
+    if (argc != 2) {
+        throw std::runtime_error("invalid command line");
+    } else if (std::ifstream ifs{argv[1]}) {
         malbolge_machine mm(ifs);
         while (!mm.exec_one_step(std::cin, std::cout)) {
+            ;
         }
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
+        return EXIT_SUCCESS;
+    } else {
+        throw std::runtime_error("can't open file");
     }
-    return EXIT_SUCCESS;
 }
