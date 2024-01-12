c505bf65a08af096114a221d8c2200207c212385
reika727 <reikasuya727@gmail.com>
2024-01-12 17:51:11 +0900

    Delete catch

--- __tests__/main.cpp
+++ __tests__/main.cpp
@@ -3,10 +3,11 @@
 #include <fstream>
 #include <stdexcept>
 
-int main(int argc, char *argv[]) try
+int main(int argc, char *argv[])
 {
     if (argc != 2) {
-        throw std::runtime_error("invalid command line");
+        std::cerr << "invalid command line" << std::endl;
+        return EXIT_FAILURE;
     } else if (std::ifstream ifs{argv[1]}) {
         malbolge_machine mm(ifs);
         while (!mm.exec_one_step(std::cin, std::cout)) {
@@ -14,12 +15,7 @@ int main(int argc, char *argv[]) try
         }
         return EXIT_SUCCESS;
     } else {
-        throw std::runtime_error("can't open file");
+        std::cerr << "can't open file" << std::endl;
+        return EXIT_FAILURE;
     }
-} catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
-} catch (...) {
-    std::cerr << "an unknown exception (a class not derived from std::exception) is caught." << std::endl;
-    return EXIT_FAILURE;
 }
